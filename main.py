58544ee66ce01e01a9851ac15bd35eacd22eff70
romaroman <chaban.roman@protonmail.com>
2019-06-25 19:46:48 +0300

    working version

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def filter_sentence_tags(tags):
 def get_soup(word):
     status_code = None
     sleep_time = 10
-
+    time.sleep(5)
     while status_code != 200:
         if sleep_time == 50:
             return None
@@ -42,13 +42,17 @@ def get_soup(word):
             time.sleep(sleep_time)
             sleep_time += 10
             logger.debug(f"Got {status_code}. Waiting for {sleep_time} seconds.")
+
         word_parts = word.split(' ')
         if word_parts[0] in ['der', 'das', 'dir']:
             req_text = '+'.join(word_parts[1:])
         else:
             req_text = word.replace(' ', '+')
         req = requests.get(LINGUEE_URL.format(req_text))
+
         status_code = req.status_code
+        if status_code != 200:
+            logger.warning(f"Faced with request status code {status_code}")
 
     return BeautifulSoup(req.text, features="lxml")
 
@@ -60,7 +64,7 @@ def scrapp_info(word):
 
     german_sentence_tags = soup.findAll("span", {"class": "tag_s"})
     english_sentence_tags = soup.findAll("span", {"class": "tag_t"})
-    english_meaning_tags = reversed(soup.findAll("span", {"class": "tag_trans"}))
+    english_meaning_tags = list(reversed(soup.findAll("span", {"class": "tag_trans"})))
 
     if not german_sentence_tags or not english_sentence_tags or not english_meaning_tags:
         return None
@@ -80,18 +84,19 @@ def scrapp_info(word):
     result = [german_entry, german_sample_sentence, ' ', english_meaning, english_secondary_meanings,
               rough_english_sentence]
     logger.info(f"Scrapped\t{' | '.join(result)}")
-    print(result)
     return result
 
 
 def main():
     file = open('words.txt', 'r+')
     words = file.read().split('\n')
-    csv = open('result2.csv', 'w+')
+    csv = open('result.csv', 'w+')
     for word in words:
         info = scrapp_info(word)
         if info:
             csv.write(",".join(info) + '\n')
+    file.close()
+    csv.close()
 
 
 if __name__ == '__main__':
